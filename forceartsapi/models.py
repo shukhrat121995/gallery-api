d36d456fee9ad3d667917cf1d31ece2f02db6874
unknown <shux121995@gmail.com>
2022-03-18 19:35:47 +0200

    fixes models.py pylint issues

--- forceartsapi/models.py
+++ forceartsapi/models.py
@@ -1,30 +1,42 @@
+"""
+This module contains models for Category, Wallpaper and ContactUs tables class representation
+There are several helper methods and classes such asa TitleField and make_file_path()
+"""
 from django.db import models
 from django.core.validators import FileExtensionValidator
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 from taggit.managers import TaggableManager
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
 
 
 class TitleField(models.CharField):
+    """This class helps to convert Wallpaper's title to lowercase"""
+
     def __init__(self, *args, **kwargs):
         super(TitleField, self).__init__(*args, **kwargs)
 
     def get_prep_value(self, value):
+        """
+        Args:
+            value: title with capital letters
+        Returns:
+            object: title in lowercase
+        """
         return str(value).lower()
 
 
 class Category(models.Model):
+    """Category class representation"""
+
     class Meta:
+        """Sets plural and singular model name"""
         verbose_name = "Category"
         verbose_name_plural = "Categories"
 
     title = TitleField(max_length=255, null=True, blank=False, unique=True)
-
     description = models.TextField(null=True, blank=False)
-
     image = models.ImageField(upload_to='characters', null=True)
-
     rank = models.IntegerField(
         unique=True,
         null=True,
@@ -32,25 +44,35 @@ class Category(models.Model):
     )
 
     def __str__(self):
+        """
+        Returns: string representation of a model
+        """
         return self.title
 
 
 def make_file_path(instance, filename):
+    """
+    Args:
+        instance:
+        filename:
+    Returns:
+        value: file path
+    """
     file_path = instance.collection.title
     return f'images/{file_path}/{filename}'
 
 
 class Wallpaper(models.Model):
+    """Wallpaper class representation"""
+
     class Meta:
+        """Sets plural and singular model name"""
         verbose_name = "Wallpaper"
         verbose_name_plural = "Wallpapers"
 
     collection = models.ForeignKey(Category, on_delete=models.CASCADE)
-
     description = models.TextField(default=None, null=True)
-
     visibility = models.BooleanField(default=True, blank=True)
-
     image = models.ImageField(
         upload_to=make_file_path,
         validators=[
@@ -58,30 +80,34 @@ class Wallpaper(models.Model):
                 allowed_extensions=[
                     'jpeg',
                     'png',
-                    'jpg'])],
+                    'jpg'
+                ]
+            )
+        ],
         null=True,
     )
-
     tags = TaggableManager()
-
-    image_thumbnail = ImageSpecField(source='image',
-                                     processors=[ResizeToFill(250, 200)],
-                                     format='JPEG',
-                                     options={'quality': 85})
-
-    upload_time = models.DateTimeField(
-        auto_now_add=True, blank=True, null=True)
-
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(250, 200)],
+        format='JPEG',
+        options={'quality': 85}
+    )
+    upload_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     views = models.IntegerField(editable=False, default=0, blank=True)
-
     likes = models.IntegerField(editable=False, default=0, blank=True)
 
     def __str__(self):
+        """
+        Returns: string representation of a model
+        """
         return self.collection.title
 
 
 class ContactUs(models.Model):
+    """ContactUs class representation"""
     class Meta:
+        """Sets plural and singular model name"""
         verbose_name = "Contact-Us"
         verbose_name_plural = "Contact-Us"
 
@@ -90,4 +116,7 @@ class ContactUs(models.Model):
     message = models.TextField(default=None, blank=True)
 
     def __str__(self):
+        """
+        Returns: string representation of a model
+        """
         return self.full_name
