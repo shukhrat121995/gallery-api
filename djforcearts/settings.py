c7a113df37b5101dccd157e7e73ade1636603621
Shukhrat <41660866+shukhrat121995@users.noreply.github.com>
2022-04-20 13:30:20 +0300

    Feature (#17)

--- djforcearts/settings.py
+++ djforcearts/settings.py
@@ -100,23 +100,23 @@ WSGI_APPLICATION = 'djforcearts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'gew',
-#         'USER': env('USER'),
-#         'PASSWORD': env('PASSWORD'),
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'sqlite',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gew',
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'sqlite',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
